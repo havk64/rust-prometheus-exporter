f77f73c39f56a4bbfea8c6b183dd8a920da57581
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2021-01-09 23:28:03 +0100

    Add first working example of prometheus exporter

---
+++ prometheus-exporter/src/main.rs
@@ -0,0 +1,52 @@
+use {
+    prometheus_exporter_base::{
+        prelude::*,
+        render_prometheus,
+    },
+    std::fs::read_dir,
+};
+
+#[derive(Debug, Clone, Default)]
+struct MyOptions {}
+
+fn calculate_file_size(path: &str) -> Result<u64, std::io::Error> {
+    let mut total_size: u64 = 0;
+    for entry in read_dir(path)? {
+        let p = entry?.path();
+        if p.is_file() {
+            total_size += p.metadata()?.len();
+        }
+    }
+
+    Ok(total_size)
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = ([0, 0, 0, 0], 32221).into();
+    println!("starting exporter on {}", addr);
+
+    render_prometheus(addr, MyOptions::default(), |request, options| async move {
+        println!(
+            "in our render_prometheus(request == {:?}, options == {:?})",
+            request, options
+        );
+
+        let total_size_log = calculate_file_size("/var/log").unwrap();
+
+        Ok(PrometheusMetric::build()
+            .with_name("folder_size")
+            .with_metric_type(MetricType::Counter)
+            .with_help("Size of the folder")
+            .build()
+            .render_and_append_instance(
+                &PrometheusInstance::new()
+                    .with_label("folder", "/var/log")
+                    .with_value(total_size_log)
+                    .with_current_timestamp()
+                    .expect("error getting the UNIX epoch"),
+            )
+            .render())
+    })
+    .await;
+}
